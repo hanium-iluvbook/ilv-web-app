4fdef0ae6c4be6914e21521444674fbed4d2f25d
한지은 <hjn510450@gmail.com>
2024-06-27 17:19:55 +0900

    ✨ feat: add color variables

---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+export const main = '#FF9F5A';
+export const darkMain = '#FF8844';
+export const pink = '#FF756C';
+export const blue = '#5AA6FF';
+export const purple = '#6A6AFF';
+export const green = '#B0DF3B';
+export const whiteGray = '#F2F3F5';
+export const lightGray = '#E8E9ED';
+export const gray = '#B5B6BD';
+export const darkGray = '#6F7075';
+export const lightBlack = '#1F2021';
+export const red = '#ED462F';
